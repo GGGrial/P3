1d9a9c0e30c16da03e7ae2908eb0e39dd90c08c0
GGGrial <GGGrial@outlook.com>
2026-03-14 16:01:39 +0800

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 
 function App() {
 	const [count, setCount] = useState(0)
 
-	return <div className="w-2 p-1 text-2xl text-slate-300">123</div>
+	return (
+		<>
+			<div className="w-2 p-1 text-2xl text-slate-300">123</div>
+			<Button>click</Button>
+		</>
+	)
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import babel from "@rolldown/plugin-babel";
-import tailwindcss from "@tailwindcss/vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		babel({ presets: [reactCompilerPreset()] }),
-	],
-});
+	plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
+})
