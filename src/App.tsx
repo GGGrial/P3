ee3461b28ad21837e8ef863c262b98791d7698a0
GGGrial <GGGrial@outlook.com>
2026-03-14 15:49:59 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return <div className="w-2 p-1 text-2xl text-slate-300">123</div>
+}
+
+export default App
