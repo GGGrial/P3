568406c2fdb1f5965997d6c148912529a5473adb
GGGrial <GGGrial@outlook.com>
2026-03-14 16:37:59 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
+import { ReactLenis } from 'lenis/react'
+import { Toaster } from '@/components/ui/sonner'
+import { toast } from 'sonner'
+import { BlurReveal } from '@/components/spell/blur-reveal'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
 		<>
-			<div className="w-2 p-1 text-2xl text-slate-300">123</div>
-			<Button>click</Button>
+			<ReactLenis root />
+			<Toaster />
+			<main className="h-500">
+				<section className="grid place-items-center">
+					<BlurReveal>click button</BlurReveal>
+					<div className="mt-50 text-2xl text-blue-300"></div>
+					<Button onClick={() => setCount(count + 1)}>click</Button>
+					<Button onClick={() => toast('Hello, world!')}>show</Button>
+				</section>
+			</main>
 		</>
 	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import path from 'node:path'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
